0bd541f25cce84db19a64ed52147b3e53a926cc6
Rodrigo Martinez <mirodrigom@gmail.com>
2018-05-25 19:01:25 -0300

    Add database.sql

--- db/supervisada.sql
+++ db/supervisada.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
--- http://www.phpmyadmin.net
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 18, 2018 at 09:14 PM
--- Server version: 5.5.59-0+deb8u1
--- PHP Version: 5.6.33-0+deb8u1
+-- Generation Time: May 25, 2018 at 07:00 PM
+-- Server version: 5.7.21
+-- PHP Version: 7.0.27-0+deb9u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,28 +14,292 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `supervisada`
 --
+DROP DATABASE IF EXISTS `supervisada`;
 CREATE DATABASE IF NOT EXISTS `supervisada` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `supervisada`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `address`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address` (
+  `idAddress` int(11) NOT NULL,
+  `idLocation` int(11) NOT NULL,
+  `address1` varchar(255) NOT NULL,
+  `address2` varchar(255) NOT NULL,
+  `postalCode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Direcciones de clientes y sucursales';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addressList`
+--
+
+DROP TABLE IF EXISTS `addressList`;
+CREATE TABLE `addressList` (
+  `idAddressList` int(11) NOT NULL,
+  `idAddress` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Lista de direcciones';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `idCategory` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Categorias';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `idCity` int(11) NOT NULL,
+  `idCountry` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Ciudades';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+CREATE TABLE `country` (
+  `idCountry` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Paises';
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`idCountry`, `name`) VALUES
+(1, 'Estados Unidos'),
+(54, 'Argentina');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cupon`
+--
+
+DROP TABLE IF EXISTS `cupon`;
+CREATE TABLE `cupon` (
+  `idCupon` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `code` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Cupones';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery`
+--
+
+DROP TABLE IF EXISTS `delivery`;
+CREATE TABLE `delivery` (
+  `idDelivery` int(11) NOT NULL,
+  `deliveryDate` varchar(255) NOT NULL,
+  `deliveryNote` varchar(255) DEFAULT NULL,
+  `deliveryTime` varchar(255) NOT NULL,
+  `deliveryPrice` float NOT NULL,
+  `deliveredDateTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Envios y Entregas';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `description`
+--
+
+DROP TABLE IF EXISTS `description`;
+CREATE TABLE `description` (
+  `idDescription` int(11) NOT NULL,
+  `length` varchar(255) DEFAULT NULL,
+  `width` varchar(255) DEFAULT NULL,
+  `prof` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Descripcion de los productos';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facture`
+--
+
+DROP TABLE IF EXISTS `facture`;
+CREATE TABLE `facture` (
+  `idFacture` int(11) NOT NULL,
+  `idOrder` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `cuitDni` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE `location` (
+  `idLocation` int(11) NOT NULL,
+  `idCity` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Localidades';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `idOrder` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `idStore` int(11) NOT NULL,
+  `idDelivery` int(11) NOT NULL,
+  `idCupon` int(11) NOT NULL,
+  `idPayment` int(11) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Pedidos - Ordenes';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderDetail`
+--
+
+DROP TABLE IF EXISTS `orderDetail`;
+CREATE TABLE `orderDetail` (
+  `idProduct` int(11) NOT NULL,
+  `idOrder` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `unitPrice` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment` (
+  `idPayment` int(11) NOT NULL,
+  `typePayment` varchar(255) NOT NULL,
+  `cuotas` int(11) NOT NULL,
+  `recharge` float NOT NULL,
+  `total` float NOT NULL,
+  `state` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pricePostalCode`
+--
+
+DROP TABLE IF EXISTS `pricePostalCode`;
+CREATE TABLE `pricePostalCode` (
+  `diffCP` int(11) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `idProduct` int(11) NOT NULL,
+  `idSubcategory` int(11) NOT NULL,
+  `idDescripcion` int(11) NOT NULL,
+  `shortDescription` varchar(255) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Productos';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+CREATE TABLE `stock` (
+  `idStore` int(11) NOT NULL,
+  `idProduct` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stock de productos';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+CREATE TABLE `store` (
+  `idStore` int(11) NOT NULL,
+  `idAddress` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Sucursales';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+DROP TABLE IF EXISTS `subcategory`;
+CREATE TABLE `subcategory` (
+  `idSubcategory` int(11) NOT NULL,
+  `idCategory` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `garantia` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `state` varchar(255) NOT NULL,
   `privilege` varchar(255) NOT NULL,
-  `created` datetime NOT NULL
+  `created` datetime NOT NULL,
+  `idAddressList` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -43,20 +307,177 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `address`
 --
-ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`,`email`);
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`idAddress`);
+
+--
+-- Indexes for table `addressList`
+--
+ALTER TABLE `addressList`
+  ADD PRIMARY KEY (`idAddressList`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`idCategory`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`idCity`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`idCountry`);
+
+--
+-- Indexes for table `cupon`
+--
+ALTER TABLE `cupon`
+  ADD PRIMARY KEY (`idCupon`);
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`idDelivery`);
+
+--
+-- Indexes for table `description`
+--
+ALTER TABLE `description`
+  ADD PRIMARY KEY (`idDescription`);
+
+--
+-- Indexes for table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`idFacture`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`idLocation`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`idOrder`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`idPayment`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`idProduct`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idStore`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`idStore`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`idSubcategory`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `address`
 --
-ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `address`
+  MODIFY `idAddress` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `addressList`
+--
+ALTER TABLE `addressList`
+  MODIFY `idAddressList` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `idCity` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `cupon`
+--
+ALTER TABLE `cupon`
+  MODIFY `idCupon` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `idDelivery` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `idFacture` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `idLocation` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `idOrder` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `idPayment` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+  MODIFY `idStore` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
